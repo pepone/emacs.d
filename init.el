ebfd7ed2384941a4c9888c6eeb19a9a72f68ea6b
Jose <jose@zeroc.com>
2016-06-29 12:43:09 +0200

    Initial emacs configuration

---
+++ init.el
@@ -0,0 +1,109 @@
+
+
+;;
+;; Melpa packages
+;;
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+(package-initialize)
+
+;;
+;; Disable the startup message
+;;
+(setq inhibit-startup-message t)
+
+
+;;
+;; activate whitespace-mode to view all whitespace characters
+;;
+(global-set-key (kbd "C-c w") 'whitespace-mode)
+
+;;
+;; show unncessary whitespace that can mess up your diff
+;;
+(add-hook 'prog-mode-hook
+          (lambda () (interactive) (setq show-trailing-whitespace 1)))
+
+;;
+;; use space to indent by default
+;;
+(setq-default indent-tabs-mode nil)
+
+;;
+;; set appearance of a tab that is represented by 8 spaces
+;;
+(setq-default tab-width 8)
+
+;;
+;; Set default C style
+;;
+(setq c-default-style "bsd"
+      c-basic-offset 4)
+
+;;
+;; SLIME
+;;
+(add-to-list 'load-path "/usr/local/share/slime")
+(require 'slime-autoloads)
+(setq slime-contribs '(slime-fancy)) ; almost everything
+(setq inferior-lisp-program "/usr/local/bin/sbcl")
+
+(put 'upcase-region 'disabled nil)
+
+;;
+;; Markdown
+;;
+(autoload 'markdown-mode "markdown-mode"
+  "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
+
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (cyberpunk)))
+ '(custom-safe-themes
+   (quote
+    ("71ecffba18621354a1be303687f33b84788e13f40141580fa81e7840752d31bf" default))))
+
+
+(require 'helm)
+(require 'helm-config)
+
+;; The default "C-x c" is quite close to "C-x C-c", which quits Emacs.
+;; Changed to "C-c h". Note: We must set "C-c h" globally, because we
+;; cannot change `helm-command-prefix-key' once `helm-config' is loaded.
+(global-set-key (kbd "C-c h") 'helm-command-prefix)
+(global-unset-key (kbd "C-x c"))
+
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
+(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
+(define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
+
+(when (executable-find "curl")
+  (setq helm-google-suggest-use-curl-p t))
+
+(setq helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
+      helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
+      helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
+      helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
+      helm-ff-file-name-history-use-recentf t)
+
+(helm-mode 1)
+
+(global-set-key (kbd "M-y") 'helm-show-kill-ring)
+(global-set-key (kbd "C-x b") 'helm-buffers-list)
+(global-set-key (kbd "M-x") #'helm-M-x)
+(global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks)
+(global-set-key (kbd "C-x C-f") #'helm-find-files)
+
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
