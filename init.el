696906b4c512bd5941cecb53bb841d05a21e1386
Jose <jose@zeroc.com>
2017-09-08 09:01:40 +0200

    Minor style fix

--- init.el
+++ init.el
@@ -86,8 +86,7 @@
     (global-set-key [remap other-window] 'ace-window)
     (custom-set-faces
      '(aw-leading-char-face
-       ((t (:inherit ace-jump-face-foreground :height 3.0)))))
-    ))
+       ((t (:inherit ace-jump-face-foreground :height 3.0)))))))
 
 (use-package flyspell
   :ensure t
