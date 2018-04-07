8afb15d0918ec75a16be42a7e87ba79c2fde1d30
Jose <jose@zeroc.com>
2018-04-07 02:06:01 +0200

    Move custom settings to ~/.emacs.d/custom.el

--- init.el
+++ init.el
@@ -226,18 +226,8 @@
 ;;  :ensure t
 ;;  :config
 ;;  (exec-path-from-shell-initialize))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (magit zenburn-theme which-key use-package try spacemacs-theme spaceline moe-theme eziam-theme color-theme base16-theme alect-themes))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
-(put 'narrow-to-region 'disabled nil)
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
+
+(provide 'init)
+;;; init.el ends here
