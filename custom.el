125c151e7cc7f841c526078f184f3a95e1b2ed7d
Jose <jose@zeroc.com>
2018-04-08 02:34:36 +0200

    Customize changes

---
+++ custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (zenburn-theme which-key vagrant-tramp use-package spaceline solarized-theme slime org-bullets markdown-mode magit intero groovy-mode gradle-mode go-mode fill-column-indicator exec-path-from-shell dracula-theme csharp-mode counsel column-enforce-mode clang-format base16-theme ace-window ace-popup-menu))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
