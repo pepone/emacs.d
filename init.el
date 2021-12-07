f561a4f5eaecebe1d5b81384ebc66db2c5d37b92
Jose <jose@zeroc.com>
2021-12-07 21:35:59 +0100

    org-roam

--- init.el
+++ init.el
@@ -365,6 +365,24 @@
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file)
 
+(use-package org-roam
+  :ensure t
+  :custom
+  (org-roam-directory (file-truename "~/knowledge"))
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+         ("C-c n f" . org-roam-node-find)
+         ("C-c n g" . org-roam-graph)
+         ("C-c n i" . org-roam-node-insert)
+         ("C-c n c" . org-roam-capture)
+         ;; Dailies
+         ("C-c n j" . org-roam-dailies-capture-today))
+  :init
+  (setq org-roam-v2-ack t)
+  :config
+  (org-roam-db-autosync-mode)
+  ;; If using org-roam-protocol
+  (require 'org-roam-protocol))
+
 (provide 'init)
 
 ;;; init.el ends here
