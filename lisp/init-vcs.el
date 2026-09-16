e53a0550f0ed0cb4a9cdcb5f10a6f82243c0a861
Jose <jose@zeroc.com>
2026-09-16 14:52:19 +0200

    Add commit spellcheck without replacing Magit defaults

--- lisp/init-vcs.el
+++ lisp/init-vcs.el
@@ -4,7 +4,8 @@
   :bind
   (("C-x g" . magit-status))
   :config
-  (setq git-commit-setup-hook '(git-commit-turn-on-flyspell)))
+  (with-eval-after-load 'git-commit
+    (add-hook 'git-commit-setup-hook #'git-commit-setup-flyspell)))
 
 ;; Forge for GitHub PR and Issue Integration
 (use-package forge
