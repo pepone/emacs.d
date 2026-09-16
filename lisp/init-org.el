9ceb36e8bc276360d3232d1ac72d0282367918ba
Jose <jose@zeroc.com>
2026-09-16 15:00:25 +0200

    Synchronize Org-roam notes and enable prose display modes

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -1,6 +1,14 @@
 ;;; init-org.el --- Org-mode and Org-Roam Setup -*- lexical-binding: t; -*-
 
+(defun my/org-roam-enable-for-notes ()
+  "Load Org-roam when visiting a file in its notes directory."
+  (when (and buffer-file-name
+             (boundp 'org-roam-directory)
+             (file-in-directory-p buffer-file-name org-roam-directory))
+    (require 'org-roam)))
+
 (use-package org-roam
+  :hook (org-mode . my/org-roam-enable-for-notes)
   :custom
   (org-roam-directory (file-truename "~/knowledge"))
   :bind (("C-c n l" . org-roam-buffer-toggle)
@@ -11,6 +19,16 @@
          ("C-c n o" . org-id-get-create)
          ("C-c n t" . org-roam-tag-add)
          ("C-c n a" . org-roam-alias-add)
-         ("C-c n j" . org-roam-dailies-capture-today)))
+         ("C-c n j" . org-roam-dailies-capture-today))
+  :config
+  (org-roam-db-autosync-mode 1))
+
+;; Match the mixed-font theme: prose wraps visually; code retains fixed pitch.
+(defun my/prose-setup ()
+  "Use proportional fonts and visual wrapping for prose."
+  (variable-pitch-mode 1)
+  (visual-line-mode 1))
+(add-hook 'org-mode-hook #'my/prose-setup)
+(add-hook 'markdown-mode-hook #'my/prose-setup)
 
 (provide 'init-org)
